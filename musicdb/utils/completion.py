6cfea917935dfa8b9b5321e684c1183e4ea0e6e2
Chris Lamb <lamby@debian.org>
2010-01-03 03:31:55 +0000

    Add a simpler completer.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/utils/completion.py
+++ musicdb/utils/completion.py
@@ -36,3 +36,34 @@ class QuerySetCompleter(object):
             return self.matching[index]
         except IndexError:
             return None
+
+class Completer(object):
+    """
+    import readline
+
+    completer = QuerySetCompleter(iterable)
+
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
+    readline.set_completer_delims('')
+
+    while 1:
+        print repr(raw_input(">>> "))
+    """
+
+    def __init__(self, iterable):
+        self.data = list(iterable)
+        self.prefix = None
+
+    def __call__(self, prefix, index):
+        # New prefix
+        if prefix != self.prefix:
+            self.matching = [
+                x for x in self.data if x.startswith(prefix)
+            ]
+            self.prefix = prefix
+
+        try:
+            return self.matching[index]
+        except IndexError:
+            return None
