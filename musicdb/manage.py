bb01882ae83f4719123f5c1f2bc20fbd53bdda89
Chris Lamb <lamby@debian.org>
2011-12-02 15:50:11 +0000

    Use our local code copies.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- musicdb/manage.py
+++ musicdb/manage.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
+
+import sys
+
+from os.path import join, dirname, abspath
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+sys.path.insert(0, join(dirname(dirname(abspath(__file__))), 'contrib'))
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+    raise
 
 if __name__ == "__main__":
     execute_manager(settings)
